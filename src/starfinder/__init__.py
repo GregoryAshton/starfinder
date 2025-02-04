9c7c71aa121f59f682aeeef56ff2f1f90828a93b
Gregory Ashton <gregory.ashton@ligo.org>
2025-02-04 09:55:15 +0000

    Add version attribute

--- src/starfinder/__init__.py
+++ src/starfinder/__init__.py
@@ -1,2 +1,7 @@
 from .logger import *
 from .starfield import StarField
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:  # development mode
+    __version__ = 'unknown'
